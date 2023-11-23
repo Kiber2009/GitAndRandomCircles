2ba272b6ae936c0da20898b3896bc85b611eca2a
Kiber2009 <alex2009game@mail.ru>
2023-11-23 18:06:37 +0300

    GitAndRandomCircles

---
+++ main.py
@@ -0,0 +1,39 @@
+from PyQt5.QtCore import QPoint
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5 import uic
+from random import randint
+import sys
+
+
+class App(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('Ui.ui', self)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_circle(qp)
+            qp.end()
+        self.do_paint = False
+
+    @staticmethod
+    def draw_circle(qp: QPainter):
+        qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
+        d = randint(10, 100)
+        qp.drawEllipse(QPoint(randint(0, 300), randint(0, 300)), d, d)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    ex.show()
+    sys.exit(app.exec())
